632e6a62611ae05b39c968be9a5e19efc1c7debd
jeakeun <136316395+jeakeun@users.noreply.github.com>
2026-04-27 11:48:12 +0900

    디자인 수정 or 초기셋팅중

--- project_react/src/App.js
+++ project_react/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [str, setStr] = useState('');
+
+    useEffect(() => {
+	    const test = async () =>{
+		    const response = await fetch('/api/test');
+
+		    if(response.ok){
+			    const result = await response.text();
+			    setStr(result);
+		    }
+	    }
+	    
+	    test(str);
+    }, []);
+    return (
+        <div className="App">
+            백엔드 데이터
+            < test/>
+        </div>
+    );
 }
 
 export default App;
