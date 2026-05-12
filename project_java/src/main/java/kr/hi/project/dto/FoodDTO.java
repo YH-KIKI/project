efa062bb78ef35a3697ad784c86abcd1679cc017
YH-KIKI <hiasis5@naver.com>
2026-05-12 09:59:42 +0900

    foodDTO 표기법 수정

--- project_java/src/main/java/kr/hi/project/dto/FoodDTO.java
+++ project_java/src/main/java/kr/hi/project/dto/FoodDTO.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 @Data
 public class FoodDTO {
-    private int Foodnum;        // 음식고유번호
-    private String Foodname;    // 음식이름
-    private int Basegram;  // 기준중량(g)
-    private float Foodkcal;     // 기준칼로리
-    private float Foodcarbs;    // 탄수화물
-    private float Foodprotein;  // 단백질
-    private float Foodfat;      // 지방
-    private float Foodnatrium;  // 나트륨
-    private String Foodtype;    // 종류 (한식 등)
+    private int foNum;        // 음식고유번호
+    private String foName;    // 음식이름
+    private int foBaseGram;  // 기준중량(g)
+    private float foKcal;     // 기준칼로리
+    private float foCarbs;    // 탄수화물
+    private float foProtein;  // 단백질
+    private float foFat;      // 지방
+    private float foNatrium;  // 나트륨
+    private String foType;    // 종류 (한식 등)
 }
