36e7a3a59981354d29cbad18ba7b871a0fba8031
jeakeun <136316395+jeakeun@users.noreply.github.com>
2026-05-14 13:33:05 +0900

    SecurityConfig 주석수정

--- project_java/src/main/java/kr/hi/project/config/SecurityConfig.java
+++ project_java/src/main/java/kr/hi/project/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                                  "/api/food/**", //[연희/추가] food 테이블 불러오는 API
                                  "/api/character/**" ,
                                  "/api/v1/bodycheck/**", // [추가/재근] 눈바디
-                                 "/api/v1/user/verify-password" // [추가/재근] 눈바디 비밀번호
+                                 "/api/v1/user/verify-password", // [추가/재근] 눈바디 비밀번호
                                  "/api/favorite/**" //[연희/추가] 즐찾들
                                  ).permitAll() 
                 .anyRequest().authenticated() 
